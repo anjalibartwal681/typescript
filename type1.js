2bcb7027c5023a09ff6b2947931e25ddf94a2e9c
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-18 16:58:24 +0000

    Create type1.js

---
+++ type1.js
@@ -0,0 +1,14 @@
+TypeScript is a statically-typed superset of JavaScript that adds optional static typing to the language.
+  It is developed and maintained by Microsoft. TypeScript code is transpiled to JavaScript code,
+  which means that it can run on any JavaScript runtime environment. The key features of TypeScript include:
+
+Static Typing:
+TypeScript introduces static typing to JavaScript, 
+  allowing developers to specify the types of variables, function parameters, and return values.
+  This helps catch type-related errors during development, providing better tooling and code intelligence.
+
+Type Annotations:
+Developers can use type annotations to explicitly specify the data types of variables, 
+  function parameters, and return types. 
+  This makes the code more self-documenting and helps IDEs provide better code completion and error checking.
+
