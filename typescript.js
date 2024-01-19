9ea05cad246f079fa897932bfc5566ef1c93de84
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-19 05:38:02 +0000

    Create typescript.js

---
+++ typescript.js
@@ -0,0 +1,12 @@
+TypeScript is  a strictly typed language. 
+  Strict typing means that variables, parameters, and other elements in the code must be declared with a specific data type, 
+  and the TypeScript compiler enforces these types during the compilation process.
+  While TypeScript does support type inference (the ability to automatically determine the type of a variable based on its initial value), 
+  it still enforces strict typing to catch type-related errors and improve code reliability.
+  Strict typing is one of the key features that distinguishes TypeScript from regular JavaScript.
+
+
+
+
+
+
