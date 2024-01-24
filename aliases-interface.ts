3b7340a6261d9ad643d65fdad10b624dd560911a
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-24 09:38:55 +0000

    Update aliases-interface.ts

--- aliases-interface.ts
+++ aliases-interface.ts
@@ -11,5 +11,32 @@ console.log(age)
 let address:Name="rishikesh";
 console.log(address)
 
+type CarYear = number;
+type CarModel= string;
+type CarType = string;
+ type Car = {
+   year:CarYear,
+   type:CarType,
+   model:CarModel
+   
+ };
+ 
+ const carYear: CarYear = 2001
+ const carModel: CarModel ="corolla";
+ const carType:CarType ="tyota"
+const car:Car = {
+     year:carYear,
+   type:carType,
+   model:carModel
+ }
+ console.log(car)
+
+
+
+
+
+
+
+
 
 
