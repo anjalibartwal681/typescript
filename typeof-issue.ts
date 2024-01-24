4c4925a1fea725d70c4a4957709c52981ef88f6e
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-24 11:49:37 +0000

    Update typeof-issue.ts

--- typeof-issue.ts
+++ typeof-issue.ts
@@ -9,8 +9,12 @@ console.log(typeof num); // Outputs: "string"
 
 // console.log(str + "2"); // Outputs: "422" (string concatenation)
 
-
+//isssue
 let num: any = 42;
 let str: string = String(num) // Type assertion using 'as' syntax
 console.log(str+4); // Outputs: "string" " 42"   give it proper 
 
+let num: any = 42;
+let str: string = num as string; // Type assertion using 'as' syntax
+
+console.log(str + 2); 
