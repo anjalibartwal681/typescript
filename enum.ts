b78c423b1db98c5ce79f33a6208aa79a6821b29f
anjali <111108636+anjalibartwal681@users.noreply.github.com>
2024-01-24 06:48:56 +0000

    Update enum.ts

--- enum.ts
+++ enum.ts
@@ -1,4 +1,19 @@
-An enum is a special "class" that represents a group of constants (unchangeable variables).
+// enum is a special type of class   that represent group of constant (unchengeble variable)
+// enum  comes in two type 
+// 1numeric  enum = by default enum will initialize value 0 and add 1 value to its next  variable,
 
-Enums come in two flavors string and numeric. Lets start with numeric.
+
+
+enum CardinalDirections {
+  North,
+  East=6,
+  South=6,
+  West
+};
+
+// logs 1 since we initialized the first value to something other than '0'
+console.log(CardinalDirections.North);
+console.log(CardinalDirections.East);
+console.log(CardinalDirections.South);
+console.log(CardinalDirections.West);
 
